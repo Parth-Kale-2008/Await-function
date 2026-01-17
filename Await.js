7b95759044b8b9e3ffd10d43afdbd777da21eb10
Parth-Kale-2008 <parthkale416@gmail.com>
2026-01-18 00:07:50 +0530

    Add files via upload

---
+++ Await.js
@@ -0,0 +1,17 @@
+function getNum() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() * 100) + 1;
+            console.log(num);
+            resolve(num);
+        }, 1000);
+    });
+}
+
+async function demo(){
+    await getNum();
+    await getNum();
+    await getNum();
+    getNum();
+}
+demo();
